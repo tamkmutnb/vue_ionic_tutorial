3190b3550743c35ca38d2f2cedd877cf0ff5cd38
Wasawat Phuangbubpha <s6101012620148@email.kmutnb.ac.th>
2021-10-05 19:45:43 +0700

    [MemortyDetails.vue] added base-layout

--- src/pages/MemoryDetails.vue
+++ src/pages/MemoryDetails.vue
@@ -1,9 +1,13 @@
 <template>
-    
+    <base-layout page-title="The Details">
+    <h2>The details page</h2>
+    </base-layout>
 </template>
 
 <script>
+import BaseLayout from '@/components/base/BaseLayout.vue'
 export default {
+  components: { BaseLayout },
     
 }
 </script>
